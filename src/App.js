9a0762f7a55c92d3a392ee5c58ad73dda0580403
Ankur Mundra <20452032+mundra-ankur@users.noreply.github.com>
2023-03-30 10:28:23 -0400

    Revert "Reusable react components and Users model (#1)"
    This reverts commit 2b04f3c9e0e197de7a4f95afcbd367aceb49274c.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Layout/Home";
-import RootLayout from "./components/Layout/Root";
-import Users from "./components/Users/Users";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "users", element: <Users /> },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
