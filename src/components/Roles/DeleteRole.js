cf1cd2dc13939a69dfc73d1321fe2c6ed6172424
palash27 <rathodpalash3@gmail.com>
2023-04-23 03:34:37 -0400

    Delete role functionality

--- src/components/Roles/DeleteRole.js
+++ src/components/Roles/DeleteRole.js
@@ -5,7 +5,33 @@ import useAPI from "../../hooks/use-api";
 import { alertActions } from "../../store/alert";
 
 const DeleteRole = ({ roleData, onClose }) => {
+  const dispatch = useDispatch();
+  const {
+    data: deletedInstitution,
+    error: institutionError,
+    sendRequest: deleteInstitution,
+  } = useAPI();
   const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    if (institutionError) {
+      dispatch(
+        alertActions.showAlert({
+          variant: "danger",
+          message: institutionError,
+        })
+      );
+    }
+  }, [institutionError, dispatch]);
+
+  useEffect(() => {
+    if (deletedInstitution.length > 0) {
+      setShow(false);
+      onClose(deletedInstitution[0]);
+    }
+  }, [deletedInstitution, onClose]);
+
+  //TODO - Delete role ?
   const deleteHandler = () => {
     console.log("Deleted");
   };
