7a27f3a1e32159bf28602326760404de3b4c869e
palash27 <rathodpalash3@gmail.com>
2023-04-25 16:06:27 -0400

    Fixed api for super administrators

--- src/components/SuperAdministrators/SuperAdministrators.js
+++ src/components/SuperAdministrators/SuperAdministrators.js
@@ -1,14 +1,14 @@
-import {useCallback, useEffect, useMemo, useState} from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
-import {useDispatch} from "react-redux";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import useAPI from "../../hooks/use-api";
-import {alertActions} from "../../store/alert";
+import { alertActions } from "../../store/alert";
 // import {AddUserIcon} from "../UI/Icons";
 import Table from "../UI/Table/Table";
 // import CreateUser from "./CreateUser";
 // import DeleteUser from "./DeleteUser";
 // import UpdateUser from "./UpdateUser";
-import {USER_ROLE_COLUMNS} from "../Administrators/userRoleColumns";
+import { USER_ROLE_COLUMNS } from "../Administrators/userRoleColumns";
 
 const Users = () => {
   const dispatch = useDispatch();
@@ -19,36 +19,39 @@ const Users = () => {
     sendRequest: fetchUsers,
   } = useAPI();
 
-  useEffect(() => fetchUsers({url: "/users", method: "get"}), [fetchUsers]);
+  useEffect(
+    () =>
+      fetchUsers({ url: "/roles/super administrator/users", method: "get" }),
+    [fetchUsers]
+  );
 
   // Error alert
   useEffect(() => {
     if (error) {
-      dispatch(alertActions.showAlert({
-        variant: "danger",
-        message: error
-      }));
+      dispatch(
+        alertActions.showAlert({
+          variant: "danger",
+          message: error,
+        })
+      );
     }
   }, [error, dispatch]);
 
-  const tableColumns = useMemo(
-    () => USER_ROLE_COLUMNS(),
-    []
-  );
+  const tableColumns = useMemo(() => USER_ROLE_COLUMNS(), []);
   const tableData = useMemo(
     () => (isLoading ? [] : userData),
     [userData, isLoading]
   );
-  
-const initialState = {hiddenColumns: ["id", "institution"]};
+
+  const initialState = { hiddenColumns: ["id", "institution"] };
 
   return (
     <Container fluid className="px-md-4">
       <Row className="mt-md-2 mb-md-2">
-        <Col md={{span: 4, offset: 4}}>
+        <Col md={{ span: 4, offset: 4 }}>
           <h1>Super Administrators</h1>
         </Col>
-        <hr/>
+        <hr />
       </Row>
       <Row>
         <Table
