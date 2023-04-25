eb0b6ef5f5a4d7a7911790fdbc62ea9bdfde8e85
palash27 <rathodpalash3@gmail.com>
2023-04-25 15:59:00 -0400

    Fetch instructors and display

--- src/components/Instructors/Instructors.js
+++ src/components/Instructors/Instructors.js
@@ -5,84 +5,62 @@ import useAPI from "../../hooks/use-api";
 import { alertActions } from "../../store/alert";
 import { AddUserIcon } from "../UI/Icons";
 import Table from "../UI/Table/Table";
-// import {USER_COLUMNS} from "./userColumns";
+import { INSTRUCTORS_COLUMNS } from "./instructorColumns";
 
 const Instructors = () => {
-  //   const dispatch = useDispatch();
-  //   const {
-  //     error,
-  //     isLoading,
-  //     data: userData,
-  //     setData: setUserData,
-  //     sendRequest: fetchUsers,
-  //   } = useAPI();
+  const dispatch = useDispatch();
+  const {
+    error,
+    isLoading,
+    data: instructorData,
+    setData: setInstructorData,
+    sendRequest: fetchInstructors,
+  } = useAPI();
 
-  //   const [showCreate, setShowCreate] = useState(false);
-  //   const [showUpdate, setShowUpdate] = useState({
-  //     visible: false,
-  //     data: {},
-  //   });
-  //   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState({
-  //     visible: false,
-  //     data: {},
-  //   });
+  useEffect(
+    () => fetchInstructors({ url: "/roles/instructor/users", method: "get" }),
+    [fetchInstructors]
+  );
 
-  //   useEffect(() => fetchUsers({url: "/users", method: "get"}), [fetchUsers]);
+  // Error alert
+  useEffect(() => {
+    if (error) {
+      dispatch(
+        alertActions.showAlert({
+          variant: "danger",
+          message: error,
+        })
+      );
+    }
+  }, [error, dispatch]);
 
-  //   // Error alert
-  //   useEffect(() => {
-  //     if (error) {
-  //       dispatch(alertActions.showAlert({
-  //         variant: "danger",
-  //         message: error
-  //       }));
-  //     }
-  //   }, [error, dispatch]);
+  const tableColumns = useMemo(() => INSTRUCTORS_COLUMNS(), []);
+  const tableData = useMemo(
+    () => (isLoading ? [] : instructorData),
+    [instructorData, isLoading]
+  );
+  const initialState = { hiddenColumns: ["id"] };
 
   return (
-    <h1>Instructors</h1>
-    // <Container fluid className="px-md-4">
-    //   <Row className="mt-md-2 mb-md-2">
-    //     <Col md={{span: 4, offset: 4}}>
-    //       <h1>Manage Users</h1>
-    //     </Col>
-    //     <hr/>
-    //   </Row>
-    //   <Row>
-    //     <Col md={{span: 1, offset: 11}}>
-    //       <Button
-    //         variant="outline-secondary"
-    //         onClick={() => setShowCreate(true)}
-    //       >
-    //         <AddUserIcon width="24" height="24"/>
-    //       </Button>
-    //     </Col>
-    //     {showCreate && <CreateUser onClose={onCreateUserHandler}/>}
-    //     {showUpdate.visible && (
-    //       <UpdateUser
-    //         userData={showUpdate.data}
-    //         onClose={onUpdateUserHandler}
-    //       />
-    //     )}
-    //     {showDeleteConfirmation.visible && (
-    //       <DeleteUser
-    //         userData={showDeleteConfirmation.data}
-    //         onClose={onDeleteUserHandler.bind(
-    //           null,
-    //           showDeleteConfirmation.data.id,
-    //           showDeleteConfirmation.data.name
-    //         )}
-    //       />
-    //     )}
-    //   </Row>
-    //   <Row>
-    //     <Table
-    //       data={tableData}
-    //       columns={tableColumns}
-    //       initialState={initialState}
-    //     />
-    //   </Row>
-    // </Container>
+    <Container fluid className="px-md-4">
+      <Row className="mt-md-2 mb-md-2">
+        <Col md={{ span: 4, offset: 4 }}>
+          <h1> Instructors</h1>
+        </Col>
+        <hr />
+      </Row>
+      <Row>
+        <Col md={{ span: 1, offset: 11 }}></Col>
+      </Row>
+      <Row>
+        {}
+        <Table
+          data={tableData}
+          columns={tableColumns}
+          initialState={initialState}
+        />
+      </Row>
+    </Container>
   );
 };
 
